c30a54d08d3a1bfe91a124c33ad94265fca0d83b
PPablo02 <100854090+PPablo02@users.noreply.github.com>
2024-11-29 13:27:34 -0600

    app_proyecto.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+from datetime import datetime, timedelta
+
+
+
+
+
+
+
