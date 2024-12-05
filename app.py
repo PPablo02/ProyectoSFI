db2fc6017eb0e08de975c0dc0202c25f5f6a3b84
Adrian Fuentes Soriano <adrian.fuentes.sor@ciencias.unam.mx>
2024-12-05 02:27:49 -0600

    “update”

--- app.py
+++ app.py
@@ -438,7 +438,8 @@ with tabs[4]:
         #drawdown, watermark = calcular_drawdown_y_watermark(precios)
 
     #Métricas por cada portafolio, hay que incluir rendimientos acumulados, sesgo, curtosis, VaR, CVAR, sharp, sortino y drowdown
-    media_min_vol = np.sum(retornos_2021_2023 * pesos_min_vol, axis=1)
+
+    media_min_vol = np.sum(retornos_2021_2023 * pesos_min_vol)
 
 
     st.write("Contenido de la variable `datos_2021_2023`:", media_min_vol)
