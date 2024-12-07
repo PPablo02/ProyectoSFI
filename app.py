07383b8b659205c4e478b603ae5c559122ac6b97
Adrian Fuentes Soriano <adrian.fuentes.sor@ciencias.unam.mx>
2024-12-06 22:19:54 -0600

    “update”

--- app.py
+++ app.py
@@ -120,21 +120,7 @@ def cargar_datos(tickers, inicio, fin):
 # --- Configuración de Streamlit ---
 st.title("Análisis de ETFs")
 
-# --- Visualización de ETFs ---
-st.header("Características de los ETFs")
-etf_caracteristicas = pd.DataFrame({
-    "Ticker": list(tickers.keys()),
-    "Nombre": [info.get("nombre", "No especificado") for info in tickers.values()],
-    "Índice": [info.get("indice", "No especificado") for info in tickers.values()],
-    "Exposición Ganada": [info.get("exposicion_ganada", "No especificado") for info in tickers.values()],
-    "Países de Inversión": [", ".join(info.get("pais_inversion", ["No especificado"])) for info in tickers.values()],
-    "Moneda": [info.get("moneda", "No especificado") for info in tickers.values()],
-    "Estilo": [info.get("estilo", "No especificado") for info in tickers.values()],
-    "Gastos (%)": [info.get("gastos", "No especificado") for info in tickers.values()],
-    "Duración": [info.get("duracion", "No especificado") for info in tickers.values()],
-    "Beta": [info.get("beta", "No especificado") for info in tickers.values()],
-    "Rendimiento YTD (%)": [info.get("rendimiento_ytd", "No especificado") for info in tickers.values()],
-})
+
 
 st.dataframe(etf_caracteristicas)
 
@@ -364,6 +350,23 @@ with tabs[1]:
     st.header("Selección de ETF's")
     hoy = datetime.today().strftime("%Y-%m-%d")
     datos_2010_hoy = cargar_datos(list(tickers.keys()), "2010-01-01", hoy)
+
+
+        # --- Visualización de ETFs ---
+    st.header("Características de los ETFs")
+    etf_caracteristicas = pd.DataFrame({
+        "Ticker": list(tickers.keys()),
+        "Nombre": [info.get("nombre", "No especificado") for info in tickers.values()],
+        "Índice": [info.get("indice", "No especificado") for info in tickers.values()],
+        "Exposición Ganada": [info.get("exposicion_ganada", "No especificado") for info in tickers.values()],
+        "Países de Inversión": [", ".join(info.get("pais_inversion", ["No especificado"])) for info in tickers.values()],
+        "Moneda": [info.get("moneda", "No especificado") for info in tickers.values()],
+        "Estilo": [info.get("estilo", "No especificado") for info in tickers.values()],
+        "Gastos (%)": [info.get("gastos", "No especificado") for info in tickers.values()],
+        "Duración": [info.get("duracion", "No especificado") for info in tickers.values()],
+        "Beta": [info.get("beta", "No especificado") for info in tickers.values()],
+        "Rendimiento YTD (%)": [info.get("rendimiento_ytd", "No especificado") for info in tickers.values()],
+    })
 
     # Crear un DataFrame consolidado con las características de los ETFs
     etf_caracteristicas = pd.DataFrame({
