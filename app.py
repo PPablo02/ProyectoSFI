f93eac550d6a0af6270b62a6c055679cabf09f11
Adrian Fuentes Soriano <adrian.fuentes.sor@ciencias.unam.mx>
2024-12-06 21:19:10 -0600

    “update”

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
 from datetime import datetime
 from scipy.optimize import minimize
 import scipy.optimize as sco
